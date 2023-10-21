b8b7ad613db2d507d0bd46b1a6e2c0ce0dfe04ea
ipriyam26 <ipriyam26@gmail.com>
2023-10-21 09:38:44 +0530

    🔨 refactor(hotel.resolver.spec.ts): improve readability and organization of tests in HotelResolver 🔨 refactor(hotel.service.ts): rename userRepository to hotleRepository for better semantics and clarity ✨ feat(hotel.resolver.spec.ts): add tests for createHotel, findAll, findOne, updateHotel, and removeHotel methods in HotelResolver ✨ feat(hotel.service.ts): update create, findOne, findAll, update, and remove methods in HotelService to use hotleRepository instead of userRepository

--- src/hotel/hotel.resolver.spec.ts
+++ src/hotel/hotel.resolver.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HotelResolver } from './hotel.resolver';
 import { HotelService } from './hotel.service';
+import { CreateHotelInput } from './dtos/create-hotel.dto';
+import { UpdateHotelInput } from './dtos/update-hotel.dto';
 
 describe('HotelResolver', () => {
   let resolver: HotelResolver;
   const mockHotelService = {
-    create: jest.fn(),
-    findAll: jest.fn(),
-    findOne: jest.fn(),
-    update: jest.fn(),
-    remove: jest.fn(),
+    create: jest
+      .fn()
+      .mockImplementation((hotelData: CreateHotelInput) => hotelData),
+    findAll: jest.fn().mockResolvedValue(['hotel1', 'hotel2']),
+    findOne: jest.fn().mockImplementation((id: number) => {
+      return { id, name: 'hotel1' };
+    }),
+    update: jest
+      .fn()
+      .mockImplementation((updateHotelInput: UpdateHotelInput) => {
+        return { id: updateHotelInput.id, ...updateHotelInput };
+      }),
+    remove: jest.fn().mockImplementation((hotelId: number) => {
+      return { id: hotelId };
+    }),
   };
 
   beforeEach(async () => {
@@ -28,5 +40,50 @@ describe('HotelResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should create a hotel', async () => {
+    const hotelData: CreateHotelInput = {
+      name: 'hotel1',
+      description: 'description',
+      address: 'address',
+      rating: 5,
+      pincode: 123456,
+      city: 'city',
+      state: 'state',
+      image_url: 'image_url',
+    };
+    expect(await resolver.createHotel(hotelData)).toEqual(hotelData);
+  });
+
+  it('should return an array of hotels', async () => {
+    expect(await resolver.findAll()).toEqual(['hotel1', 'hotel2']);
+  });
+  it('should return a hotel', async () => {
+    expect(await resolver.findOne(1)).toEqual({
+      id: 1,
+      name: 'hotel1',
+    });
+  });
+
+  it('should update a hotel', async () => {
+    const updateHotelInput: UpdateHotelInput = {
+      id: 1,
+      name: 'hotel1',
+      description: 'description',
+      address: 'address',
+      rating: 5,
+      pincode: 123456,
+      city: 'city',
+      state: 'state',
+      image_url: 'image_url',
+    };
+    expect(await resolver.updateHotel(updateHotelInput)).toEqual(
+      updateHotelInput,
+    );
+  });
+
+  it('should remove a hotel', async () => {
+    expect(await resolver.removeHotel(1)).toEqual({ id: 1 });
   });
 });
--- src/hotel/hotel.service.ts
+++ src/hotel/hotel.service.ts
@@ -10,33 +10,33 @@ import { UpdateHotelInput } from './dtos/update-hotel.dto';
 export class HotelService {
   constructor(
     @InjectRepository(Hotel)
-    private readonly userRepository: Repository<Hotel>,
+    private readonly hotleRepository: Repository<Hotel>,
   ) {}
 
   async create(createHotelInput: CreateHotelInput): Promise<Hotel> {
-    this.userRepository.create(createHotelInput);
-    return await this.userRepository.save(createHotelInput);
+    this.hotleRepository.create(createHotelInput);
+    return await this.hotleRepository.save(createHotelInput);
   }
   async findOne(id: number): Promise<Hotel> {
-    return await this.userRepository.findOne({
+    return await this.hotleRepository.findOne({
       where: [{ id }],
     });
   }
   async findAll(): Promise<Hotel[]> {
-    return await this.userRepository.find();
+    return await this.hotleRepository.find();
   }
   async update(updateHotelInput: UpdateHotelInput): Promise<Hotel> | undefined {
     const hotel = await this.findOne(updateHotelInput.id);
     if (!hotel) {
       throw new NotFoundException(`Hotel #${updateHotelInput.id} not found`);
     }
-    return await this.userRepository.save(updateHotelInput);
+    return await this.hotleRepository.save(updateHotelInput);
   }
   async remove(id: number): Promise<Hotel> {
     const hotel = await this.findOne(id);
     if (!hotel) {
       throw new NotFoundException(`Hotel #${id} not found`);
     }
-    return await this.userRepository.remove(hotel);
+    return await this.hotleRepository.remove(hotel);
   }
 }
