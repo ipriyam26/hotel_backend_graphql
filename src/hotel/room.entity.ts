291a1c47e02d3999f62d23cb5f191eb7f7042035
ipriyam26 <ipriyam26@gmail.com>
2023-10-23 09:50:32 +0530

    🔀 refactor(room.entity.ts): improve code readability and add GraphQL decorators to Room entity
    - Add `@ObjectType()` decorator to Room entity to indicate it's a GraphQL object type
    - Add `@Field()` decorator to expose fields to GraphQL schema
    - Import `ID`, `Float`, and `Int` types from `@nestjs/graphql` for GraphQL compatibility
    - Rearrange imports for better organization and readability

--- src/hotel/room.entity.ts
+++ src/hotel/room.entity.ts
@@ -1,38 +1,48 @@
+import { Field, ObjectType, ID, Float, Int } from '@nestjs/graphql';
 import {
   Entity,
-  Column,
   PrimaryGeneratedColumn,
-  CreateDateColumn,
-  UpdateDateColumn,
+  Column,
   ManyToOne,
   JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 import { Hotel } from './hotel.entity';
 
+@ObjectType()
 @Entity('rooms')
 export class Room {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(() => Int) // Import Int from '@nestjs/graphql'
   @Column({ type: 'int' })
   hotel_id: number;
 
+  @Field(() => Hotel)
   @ManyToOne(() => Hotel)
   @JoinColumn({ name: 'hotel_id' })
   hotel: Hotel;
 
+  @Field()
   @Column({ type: 'varchar', length: 50 })
   type: string;
 
+  @Field(() => Float)
   @Column({ type: 'float' })
   price: number;
 
+  @Field()
   @Column({ type: 'boolean' })
   availability: boolean;
 
+  @Field()
   @CreateDateColumn()
   created_at: Date;
 
+  @Field()
   @UpdateDateColumn()
   updated_at: Date;
 }
